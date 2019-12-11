b1045de76ce968988f76c067702928130d26a995
Jacob Wells <jpwells0105@hotmail.com>
2019-12-11 15:50:39 -0800

    Second

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,7 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import 
+import frc.robot.commands.TankDrive;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -21,7 +22,7 @@ public class OI {
   public Button CON1 = new JoystickButton(CON, 1);
 
   public OI(){ 
-CON1.whileHeld(new TankDrive);
+CON1.whileHeld(new TankDrive());
 }
   
   // One type of button is a joystick button which is any button on a
